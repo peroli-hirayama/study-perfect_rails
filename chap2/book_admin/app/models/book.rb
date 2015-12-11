155dd79942c211a125e7b9515cdb8bbf3820d79e
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-11 16:38:56 +0900

    disable default scope

--- chap2/book_admin/app/models/book.rb
+++ chap2/book_admin/app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
     scope :costly, -> { where("price > ?", 3000) }
     scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
-    default_scope -> { order("published_on desc") }
+#    default_scope -> { order("published_on desc") }
 end
