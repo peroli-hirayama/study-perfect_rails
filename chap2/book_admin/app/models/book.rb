e854d184a4806cf1ea383a6a1f6ad457056e5f91
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-11 17:24:51 +0900

    add validations for Book model

--- chap2/book_admin/app/models/book.rb
+++ chap2/book_admin/app/models/book.rb
@@ -7,4 +7,13 @@ class Book < ActiveRecord::Base
 
     has_many :book_authors
     has_many :authors, through: :book_authors
+
+    validates :name, presence: true
+    validates :name, length: { maximum: 15 }
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validate do |book|
+        if book.name.include?("exercise")
+            book.errors[:name] << "I don't like exercise."
+        end
+    end
 end
