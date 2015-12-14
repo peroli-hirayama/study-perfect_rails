02191331068fea0fa1920dc7e3640e2ec4a97e93
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-14 14:17:42 +0900

    add strong parameter settings to use user

--- chap2/book_admin/app/controllers/profile_controller.rb
+++ chap2/book_admin/app/controllers/profile_controller.rb
@@ -6,5 +6,14 @@ class ProfileController < ApplicationController
   end
 
   def update
+
+      user = current_user
+      # params[:user] => {name: "bob", email: "bob@example.com"}
+      user.update(user_params)
+  end
+  
+  private
+  def user_params
+    param.require(:user).permit(:name, :email)
   end
 end
