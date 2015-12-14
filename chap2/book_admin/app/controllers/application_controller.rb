6897bdf35f35ba80c8d7313f71ee21854a182858
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-14 12:22:27 +0900

    add basic action callbacks

--- chap2/book_admin/app/controllers/application_controller.rb
+++ chap2/book_admin/app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action do
+    redirect_to access_denied_path if params[:token].blank?
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
