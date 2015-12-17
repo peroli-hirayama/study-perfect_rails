9b27b19eb025f97a4ef5bd818fd07306c5766db4
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-17 18:32:50 +0900

    add usage sample of active_decorator

--- chap4/active_record_usage/app/decorators/user_decorator.rb
+++ chap4/active_record_usage/app/decorators/user_decorator.rb
@@ -1,3 +1,10 @@
 # coding: utf-8
 module UserDecorator
+    def email_link(body = nil, &block)
+        if block_given?
+            link_to("mailto:#{email}", &block)
+        else
+            link_to(body, "mailto:#{email}")
+        end
+    end
 end
