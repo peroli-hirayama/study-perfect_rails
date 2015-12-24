b4585a7d70752565dac31786af120aa7d608735e
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-24 17:09:02 +0900

    add constraint between user and event

--- chap6/awesome_events/app/controllers/users_controller.rb
+++ chap6/awesome_events/app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
     def retire
         @current_user = current_user
     end
+
+    def destroy
+        if current_user.destroy
+            reset_session
+            redirect_to root_path, notice: '退会完了しました'
+        else
+            render :retire
+        end
+    end
 end
