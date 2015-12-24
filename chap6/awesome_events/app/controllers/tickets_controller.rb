481d079a33e05ae92ca3e5409e8e8b5130fd3b65
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-24 14:37:20 +0900

    add async process to create ticket

--- chap6/awesome_events/app/controllers/tickets_controller.rb
+++ chap6/awesome_events/app/controllers/tickets_controller.rb
@@ -1,2 +1,20 @@
 class TicketsController < ApplicationController
+    before_action :authenticate
+
+    def new
+        raise ActionController::RoutingError, 'ログイン状態で TicketsController#new にアクセスされました'
+    end
+
+    def create
+        ticket = current_user.tickets.build do |t|
+            t.event_id = params[:event_id]
+            t.comment = params[:ticket][:comment]
+        end
+        if ticket.save
+            flash[:notice] = 'このイベントに参加表明しました'
+            head 201 # 201 Created
+        else
+            render json: { messages: ticket.errors.full_messages }, status: 422 # validatorによりfull_messagesを取得: 422 Unprocessable Entity
+        end
+    end
 end
