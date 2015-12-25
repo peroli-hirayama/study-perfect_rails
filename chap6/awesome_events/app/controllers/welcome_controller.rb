049f0a87892ef0c0c2093a3722dadc577a798590
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-25 14:20:03 +0900

    add pagination feature with kaminari

--- chap6/awesome_events/app/controllers/welcome_controller.rb
+++ chap6/awesome_events/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  PER = 10
+  PER = 5
 
   def index
       @events = Event.page(params[:page]).per(PER).where('start_time > ?', Time.zone.now).order(:start_time)
