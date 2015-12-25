86123700511c9da88e959a81161c9b37d814e666
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-12-25 14:58:40 +0900

    use ransack to search models

--- chap6/awesome_events/app/controllers/welcome_controller.rb
+++ chap6/awesome_events/app/controllers/welcome_controller.rb
@@ -2,6 +2,18 @@ class WelcomeController < ApplicationController
   PER = 5
 
   def index
-      @events = Event.page(params[:page]).per(PER).where('start_time > ?', Time.zone.now).order(:start_time)
+      # search_params 例：
+      # {"name_cont"=>"ランチ", "start_time_gteq(1i)"=>"2015", "start_time_gteq(2i)"=>"12", "start_time_gteq(3i)"=>"25"}
+      # .search()のところで、Ransack内の命名規則に従ってSQLのwhere句に変換されるっぽい
+      @q = Event.page(params[:page]).per(PER).order(:start_time).search(search_params)
+      @events = @q.result(distinct: true)
+  end
+
+  private
+
+  def search_params
+      params.require(:q).permit! # Strong parameter
+  rescue
+      { start_time_gteq: Time.zone.now }
   end
 end
