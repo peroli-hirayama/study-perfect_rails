afe8406cd925af1df7282b9d8cee278043c4be29
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2016-01-07 18:16:30 +0900

    move site-cookbooks

--- chap8/rails_book_cookbook/site-cookbooks/rails_book_cookbook/metadata.rb
+++ chap8/rails_book_cookbook/site-cookbooks/rails_book_cookbook/metadata.rb
@@ -5,3 +5,5 @@ license          'MIT License'
 description      'A cookbook for rails_book'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
+#
+depends          'rbenv'
