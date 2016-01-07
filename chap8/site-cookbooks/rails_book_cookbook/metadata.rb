1851e555ed4905b0dc5df0d45d4f2f54d57c0bd9
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2016-01-07 17:40:16 +0900

    create custom recipe

---
+++ chap8/site-cookbooks/rails_book_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'rails_book_cookbook'
+maintainer       'Takayuki HIRAYAMA'
+maintainer_email 'takayuki.hirayama@peroli.jp'
+license          'MIT License'
+description      'A cookbook for rails_book'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.2'
